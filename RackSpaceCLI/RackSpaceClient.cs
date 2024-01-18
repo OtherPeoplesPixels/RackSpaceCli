e14d8408646123c83760219975cf1f5b72e817b7
drewb8L <104442990+drewb8L@users.noreply.github.com>
2024-01-18 15:59:33 -0600

    Refactor and cleanup of RackSpaceClient class

--- RackSpaceCLI/RackSpaceClient.cs
+++ RackSpaceCLI/RackSpaceClient.cs
@@ -1,5 +1,3 @@
-using System;
-using System.IO;
 using System.Net;
 using System.Security.Cryptography;
 using System.Text;
@@ -7,96 +5,94 @@ using System.Text;
 namespace RackSpaceCLI;
 
 public class RestApiClient
+{
+    private readonly string apiKey;
+    private readonly string baseUrl;
+    private HttpWebRequest request;
+    private readonly string secretKey;
+
+    public RestApiClient(string baseUrl, string apiKey, string secretKey)
     {
-        private HttpWebRequest request;
-        // private HttpWebResponse response;
-        private string baseUrl;
-        private string apiKey;
-        private string secretKey;
+        this.baseUrl = baseUrl;
+        this.apiKey = apiKey;
+        this.secretKey = secretKey;
+    }
 
-        public RestApiClient(string baseUrl, string apiKey, string secretKey)
+    public async Task<HttpWebResponse> Get(string url, string format)
+    {
+        try
         {
-            this.baseUrl = baseUrl;
-            this.apiKey = apiKey;
-            this.secretKey = secretKey;
-        }
-
-        public async Task<HttpWebResponse> Get(string url, string format)
-        {
-            try
-            {
-                request = (HttpWebRequest)HttpWebRequest.Create(this.baseUrl + url);
-                request.Method = "GET";
-                SignMessage();
-                AssignFormat(format);
-                return await GetResponseContentAsync();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-        }
-
-        public async Task<HttpWebResponse> Post(string url, string data, string format)
-        {
-            request = (System.Net.HttpWebRequest)HttpWebRequest.Create(baseUrl + url);
-            request.Method = "POST";
-            SignMessage();
-            AssignFormat(format);
-            SendFormData(data);
-            return await GetResponseContentAsync();
-        }
-        
-
-        public async Task<HttpWebResponse> Delete(string url, string format)
-        {
-            request = (System.Net.HttpWebRequest)HttpWebRequest.Create(baseUrl + url);
-            request.Method = "DELETE";
+            request = (HttpWebRequest)WebRequest.Create(baseUrl + url);
+            request.Method = "GET";
             SignMessage();
             AssignFormat(format);
             return await GetResponseContentAsync();
         }
-
-        private void SendFormData(string data)
+        catch (Exception e)
         {
-            UTF8Encoding encoding = new UTF8Encoding();
-            byte[] byteData = encoding.GetBytes(data);
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.ContentLength = byteData.Length;
-            Stream requestStream = request.GetRequestStream();
-            requestStream.Write(byteData, 0, byteData.Length);
-            requestStream.Close();
-        }
-
-        private async Task<HttpWebResponse> GetResponseContentAsync()
-        {
-            try
-            {
-                return (HttpWebResponse)await request.GetResponseAsync();
-            }
-            catch (WebException e)
-            {
-                return (HttpWebResponse)e.Response;
-            }
-
-        }
-
-        private void SignMessage()
-        {
-            var userAgent = "C# Client Library";
-            request.UserAgent = userAgent;
-            var dateTime = DateTime.UtcNow.ToString("yyyyMMddHHmmss");
-            var dataToSign = apiKey + userAgent + dateTime + secretKey;
-            var hash = SHA1.Create();
-            var signedBytes = hash.ComputeHash(Encoding.UTF8.GetBytes(dataToSign));
-            var signature = Convert.ToBase64String(signedBytes);
-
-            request.Headers["X-Api-Signature"] = apiKey + ":" + dateTime + ":" + signature;
-        }
-
-        private void AssignFormat(string format)
-        {
-            this.request.Accept = format;
+            Console.WriteLine(e);
+            throw;
         }
     }
+
+    public async Task<HttpWebResponse> Post(string url, string data, string format)
+    {
+        request = (HttpWebRequest)WebRequest.Create(baseUrl + url);
+        request.Method = "POST";
+        SignMessage();
+        AssignFormat(format);
+        SendFormData(data);
+        return await GetResponseContentAsync();
+    }
+
+
+    public async Task<HttpWebResponse> Delete(string url, string format)
+    {
+        request = (HttpWebRequest)WebRequest.Create(baseUrl + url);
+        request.Method = "DELETE";
+        SignMessage();
+        AssignFormat(format);
+        return await GetResponseContentAsync();
+    }
+
+    private void SendFormData(string data)
+    {
+        var encoding = new UTF8Encoding();
+        var byteData = encoding.GetBytes(data);
+        request.ContentType = "application/x-www-form-urlencoded";
+        request.ContentLength = byteData.Length;
+        var requestStream = request.GetRequestStream();
+        requestStream.Write(byteData, 0, byteData.Length);
+        requestStream.Close();
+    }
+
+    private async Task<HttpWebResponse> GetResponseContentAsync()
+    {
+        try
+        {
+            return (HttpWebResponse)await request.GetResponseAsync();
+        }
+        catch (WebException e)
+        {
+            return (HttpWebResponse)e.Response;
+        }
+    }
+
+    private void SignMessage()
+    {
+        var userAgent = "C# Client Library";
+        request.UserAgent = userAgent;
+        var dateTime = DateTime.UtcNow.ToString("yyyyMMddHHmmss");
+        var dataToSign = apiKey + userAgent + dateTime + secretKey;
+        var hash = SHA1.Create();
+        var signedBytes = hash.ComputeHash(Encoding.UTF8.GetBytes(dataToSign));
+        var signature = Convert.ToBase64String(signedBytes);
+
+        request.Headers["X-Api-Signature"] = apiKey + ":" + dateTime + ":" + signature;
+    }
+
+    private void AssignFormat(string format)
+    {
+        request.Accept = format;
+    }
+}
